c2d5d9f9dd3db1c6ca4032542b1d7cd21a08bd53
damoski2 <akogunoyindamola42@gmail.com>
2021-10-07 12:21:38 +0100

    completion

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,14 +1,192 @@
-import styled from 'styled-components'
+import { useState, useRef } from "react";
+import styled from "styled-components";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
+import { useRouter } from "next/router";
+import { Avatar, IconButton } from "@material-ui/core";
+import { MoreVert } from "@material-ui/icons";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
+import { useCollection } from "react-firebase-hooks/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import Message from "./Message";
+import getRecipientEmail from "../utils/getRecipientEmail";
+import TimeAgo from 'timeago-react';
 
-const ChatScreen = () => {
-    return (
-        <Container>
-            This is a chat
-        </Container>
-    )
-}
 
-export default ChatScreen
+const ChatScreen = ({ chat, messages }) => {
+  //console.log(chat, messages);
+  const [user] = useAuthState(auth);
+  const [input, setInput] = useState("");
+  const endOfMessagesRef = useRef(null)
+  const router = useRouter();
+  const [messagesSnapShot] = useCollection(
+    db
+      .collection("chats")
+      .doc(router.query.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
 
+  const [recipientSnapShot] = useCollection(
+    db
+      .collection("users")
+      .where("email", "==", getRecipientEmail(chat.users, user))
+  );
 
-const Container = styled.div``
+  const showMessages = () => {
+    if (messagesSnapShot) {
+      return messagesSnapShot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    } else {
+      return JSON.parse(messages).map((message) => (
+        <Message key={message.id} user={message.user} message={message} />
+      ));
+    }
+  };
+
+  const scrollToBottom = ()=>{
+    endOfMessagesRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    })
+  }
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    db.collection("users").doc(user.uid).set(
+      {
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+      },
+      { merge: true }
+    );
+
+    db.collection("chats").doc(router.query.id).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user.email,
+      photoURL: user.photoURL,
+    });
+
+    setInput("");
+    scrollToBottom();
+  };
+
+  const recipientEmail = getRecipientEmail(chat.users, user);
+  const recipient = recipientSnapShot?.docs?.[0].data();
+
+  return (
+    <Container>
+      <Header>
+        {recipient ? (
+          <Avatar src={recipient?.photoURL} />
+        ) : (
+          <Avatar src={recipientEmail[0]} />
+        )}
+        <HeaderInformation>
+          <h3>{recipientEmail}</h3>
+          {recipientSnapShot ? <p>Last Active: {' '} {recipient?.lastSeen?.toDate()?(
+              <TimeAgo datetime={recipient?.lastSeen?.toDate()} />
+          ) : (
+              "Unavailable"
+          )} </p> : <p> Loading Last Active... </p>}
+        </HeaderInformation>
+        <HeaderIcon>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </HeaderIcon>
+      </Header>
+
+      <MessageContainer>
+        {showMessages()}
+        <EndOfMessage ref={endOfMessagesRef} />
+      </MessageContainer>
+
+      <InputContainer>
+        <InsertEmoticonIcon />
+        <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button hidden disabled={!input} type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
+        <MicIcon />
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default ChatScreen;
+
+const Container = styled.div``;
+
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+
+const HeaderIcon = styled.div``;
+
+const EndOfMessage = styled.div`
+    margin-bottom: 50px;
+`;
+
+const MessageContainer = styled.div`
+  padding: 30px;
+  background-color: #e5ded8;
+  min-height: 90vh;
+`;
+
+const InputContainer = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  position: sticky;
+  bottom: 0;
+  background-color: white;
+  z-index: 100;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  background-color: whitesmoke;
+  outline: 0;
+  border: none;
+  border-radius: 10px;
+  padding: 20px;
+  margin-left: 15px;
+  margin-right: 15px;
+`;
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -2,18 +2,24 @@ import styled from 'styled-components'
 import Head from 'next/head'
 import Sidebar from '../../components/Sidebar'
 import ChatScreen from '../../components/ChatScreen'
-import { db } from '../../firebase'
+import { db, auth } from '../../firebase'
+import { useAuthState } from "react-firebase-hooks/auth"
+import getRecipientEmail from '../../utils/getRecipientEmail'
 
 
 const Chat = ({ chat, messages }) => {
+
+    const [user] = useAuthState(auth)
+    console.log(chat)
+
     return (
         <Container>
             <Head>
-                <title>Chat</title>
+                <title>Chat with {getRecipientEmail(chat.users, user)} </title>
             </Head>
             <Sidebar />
             <ChatContainer>
-                <ChatScreen />
+                <ChatScreen chat={chat} messages={messages} />
             </ChatContainer>
         </Container>
     )
@@ -26,7 +32,7 @@ export const getServerSideProps = async(ctx)=>{
     const ref = db.collection("chats").doc(ctx.query.id)
 
     //Prep the messages on the server
-    const messagesRes = await ref.collection('messages').order('timestamp','asc').get();
+    const messagesRes = await ref.collection('messages').orderBy('timestamp','asc').get();
 
     const messages = messagesRes.docs.map(doc=>({
         id: doc.id,
